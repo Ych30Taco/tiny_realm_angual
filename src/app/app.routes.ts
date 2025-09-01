e0c56aa492ba53c1cbd76e769799b27d65919861
J1140065 <j1140065@tssco.com.tw>
2025-09-01 11:19:02 +0800

    路由器添加

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
+import { Main } from './tab/main/main';
+import { Player } from './tab/player/player';
+import { Resource } from './tab/resource/resource';
+import { Building } from './tab/building/building';
+import { Soldier } from './tab/soldier/soldier';
+import { TerrainMap } from './tab/terrain-map/terrain-map';
+import { Battle } from './tab/battle/battle';
+import { DataAdmin } from './tab/data-admin/data-admin';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: 'main',
+    component: Main
+  },
+  {
+    path: 'player',
+    component: Player
+  },
+  {
+    path: 'resource',
+    component: Resource
+  },
+  {
+    path: 'building',
+    component: Building
+  },
+  {
+    path: 'soldier',
+    component: Soldier
+  },
+  {
+    path: 'terrainMap',
+    component: TerrainMap
+  },
+  {
+    path: 'battle',
+    component: Battle
+  },
+  {
+    path: 'dataAdmin',
+    component: DataAdmin
+  },
+  {
+    path: '',
+    redirectTo: '/main',
+    pathMatch: 'full'
+  }
+];
